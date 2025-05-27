3d6b22e0ee7636b835bf627ce951438c064c0638
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-27 17:00:19 +0100

    attempting to make db playbook work

--- tf-create-basic-ec2/main.tf
+++ tf-create-basic-ec2/main.tf
@@ -67,6 +67,27 @@ resource "aws_instance" "tech504-callum-ubuntu-2204-ansible-target-node-app" {
     }
 }
 
+resource "aws_instance" "tech504-callum-ubuntu-2204-ansible-target-node-database" {
+    # which image do we use? - on AWS called an AMI for some reason.
+    # what is AMI ID? ami-0c1c30571d2dae5c9 (ubuntu 22.04lts)
+    ami            = var.app_ami_id    
+
+    # which instance type - t3.micro
+    instance_type = var.machine
+    
+    # do we want a public IP?
+    associate_public_ip_address = false
+
+    vpc_security_group_ids = [aws_security_group.tech504-callum-ansible-db-rules.id]
+
+    key_name = data.aws_key_pair.tech504-callum-aws.key_name
+    
+    # instance name
+    tags = {
+        Name = "tech504-callum-ubuntu-2204-ansible-target-node-database"
+    }
+}
+
 # Using Terraform and Terraform official documentation:
 # Create an AWS security group named techXXX-firstname-tf-allow-port-22-3000-80 (tf so you know it was created by Terraform)
 resource "aws_security_group" "tech504-callum-ansible-ssh-only" {
@@ -89,91 +110,90 @@ resource "aws_security_group" "tech504-callum-ansible-ssh-http-3000" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow-ssh-ipv4-only" {
+resource "aws_security_group" "tech504-callum-ansible-db-rules" {
 
-  security_group_id = aws_security_group.tech504-callum-ansible-ssh-only.id
+  name = "tech504-callum-ansible-db-rules"
+  description = "allows ssh and 27017"
 
+  tags = {
+    Name = "tech504-callum-tf-allow-ssh-27017"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow-ssh-ipv4-again" {
+  security_group_id = aws_security_group.tech504-callum-ansible-db-rules.id
   cidr_ipv4         = var.CIDR
-
-  ip_protocol       = "tcp"
-  
+  ip_protocol       = "tcp" 
   from_port         = 22
+  to_port           = 22
+}
 
+resource "aws_vpc_security_group_ingress_rule" "allow-db-traffic" {
+  security_group_id = aws_security_group.tech504-callum-ansible-db-rules.id
+  cidr_ipv4         = var.CIDR
+  ip_protocol       = "tcp" 
+  from_port         = 27017
+  to_port           = 27017
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow-ssh-ipv4-only" {
+  security_group_id = aws_security_group.tech504-callum-ansible-ssh-only.id
+  cidr_ipv4         = var.CIDR
+  ip_protocol       = "tcp" 
+  from_port         = 22
   to_port           = 22
 }
 
 #   Allow port 22 from localhost
 resource "aws_vpc_security_group_ingress_rule" "allow-ssh-ipv4" {
-
   security_group_id = aws_security_group.tech504-callum-ansible-ssh-http-3000.id
-
   cidr_ipv4         = var.CIDR
-
   ip_protocol       = "tcp"
-  
   from_port         = 22
-
   to_port           = 22
 }
 
 #   Allow port 80 from all
 resource "aws_vpc_security_group_ingress_rule" "allow-http-ipv4" {
-
   security_group_id = aws_security_group.tech504-callum-ansible-ssh-http-3000.id
-
   cidr_ipv4         = var.CIDR
-
   ip_protocol       = "tcp"
-
   from_port         = 80
-
   to_port           = 80
-
 }
 
 #   Allow port 3000 from all
 resource "aws_vpc_security_group_ingress_rule" "allow-tcp-3000" {
-
   security_group_id = aws_security_group.tech504-callum-ansible-ssh-http-3000.id
-
   cidr_ipv4         = var.CIDR
-
   ip_protocol       = "tcp"
-
   from_port         = 3000
-
   to_port           = 3000
-
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow-all-egress-1" {
-
   security_group_id = aws_security_group.tech504-callum-ansible-ssh-http-3000.id
-
   cidr_ipv4         = var.CIDR
-
   ip_protocol       = -1
-
   from_port         = 0
-
   to_port           = 0
-
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow-all-egress-2" {
-
   security_group_id = aws_security_group.tech504-callum-ansible-ssh-only.id
-
   cidr_ipv4         = var.CIDR
-
   ip_protocol       = -1
-
   from_port         = 0
-
   to_port           = 0
-
 }
 
+resource "aws_vpc_security_group_egress_rule" "allow-all-egress-3" {
+  security_group_id = aws_security_group.tech504-callum-ansible-db-rules.id
+  cidr_ipv4         = var.CIDR
+  ip_protocol       = -1
+  from_port         = 0
+  to_port           = 0
+}
 
 
 #   Modify the EC2 instance created in main.tf:
